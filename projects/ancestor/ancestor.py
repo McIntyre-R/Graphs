d5a64bf93c68d36447e4d6e761161fd1731adb8e
Ronald McIntyre <r.campbell.mcintyre@gmail.com>
2020-08-12 15:49:56 -0400

    ancestor

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,17 @@
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    unexplored = [[starting_node],]
+    while len(unexplored) > 0:
+        cur =  unexplored.pop()
+        for e in ancestors:
+            if e[1] == cur[len(cur)-1]:
+                unexplored.append([*cur,e[0]])
+    if len(cur) != 1:
+        return cur[len(cur)-1]
+    else:
+        return -1
+
+            
+
+
+
